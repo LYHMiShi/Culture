0c306e5f14d063c779ba2fc312b2670e6fcee3ed
LYHMiShi <1693208985@qq.com>
2017-09-26 00:34:25 +0800

    wenyuhangs

--- app/src/main/java/culture/culture/MainActivity.java
+++ app/src/main/java/culture/culture/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-//545153250552
+//545153250552jdkfhjssds
 public class MainActivity extends AppCompatActivity {
 
     @Override
